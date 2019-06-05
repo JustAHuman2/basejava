989fb1fb66777c7f4341d095d8db267dc24eb4db
Stepanov Dmitry <zorg2@bk.ru>
2019-06-05 18:46:35 +0300

    refactored methods, removed comparator, equals and geter

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,93 +1,51 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 
-/**
- * Array based storage for Resumes
- */
 public class ArrayStorage {
+
+    private int size;
 
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            storage[i] = null;
+        Arrays.fill(storage, null);
+        size = 0;
+    }
+
+    void save(Resume r) {
+        for (int i = 0; i < size; i++) {
+            if (r.toString() == storage[i].toString()) {
+               return;
+            }
+        }
+        storage[size] = r;
+        size++;
+    }
+
+    void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].toString()) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                return;
+            }
         }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if ((null != storage[i]) && (uuid == (storage[i].getUuid()))) {
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].toString()) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        ArrayList<Resume> resumes = new ArrayList<>();
-        for (int i = 0; i < storage.length; i++) {
-            if (null != storage[i]) {
-                resumes.add(storage[i]);
-            }
-        }
-        Resume[] result = new Resume[resumes.size()];
-        for (int i = 0; i < resumes.size(); i++) {
-            result[i] = resumes.get(i);
-        }
-        return result;
-    }
-
-    void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if ((null != storage[i]) && (r.equals(storage[i]))) {
-                return;
-            }
-        }
-        for (int i = 0; i < storage.length; i++) {
-            if (null == storage[i]) {
-                storage[i] = r;
-                Arrays.sort(storage, new ResumeSort());
-                break;
-            }
-        }
-    }
-
-    void delete(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if ((null != storage[i]) && (uuid == (storage[i].getUuid()))) {
-                storage[i] = null;
-                Arrays.sort(storage, new ResumeSort());
-                break;
-            }
-        }
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int count = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (null != storage[i]) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public class ResumeSort implements Comparator<Resume> {
-        public int compare(Resume left, Resume right) {
-            if (((left != null) & (right != null)) || ((left == null) & (right == null))) {
-                return 0;
-            }
-            if ((left != null) & (right == null)) {
-                return 1;
-            }
-            if ((left == null) & (right != null)) {
-                return -1;
-            }
-            return compare(left, right);
-        }
+        return size;
     }
 }
--- src/Resume.java
+++ src/Resume.java
@@ -1,25 +1,9 @@
-/**
- * Initial resume class
- */
 public class Resume {
 
-    // Unique identifier
     String uuid;
-
-   public String getUuid (){
-        return uuid;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        Resume otherResume = (Resume) obj;
-        return this.uuid == otherResume.uuid;
-    }
 
     @Override
     public String toString() {
         return uuid;
     }
 }
-
-
